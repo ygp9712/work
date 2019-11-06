6b27294656fe18307af1d5a45d7909eab47d3265
Yang <1175430716@qq.com>
2019-11-06 17:15:32 +0800

    快速搭建

---
+++ src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="all">
+  <div class="left">
+    </div>
+      <div class="right">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
